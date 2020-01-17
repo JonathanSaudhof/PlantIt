aa562d87f4d33036d3ff8ebf7ade8e70a1743c9a
jonathansaudhof <jonathan@jonathansaudhof.de>
2020-01-17 16:35:07 +0100

    initil commit

---
+++ src/js/_models.js
@@ -0,0 +1,5 @@
+export class Game {
+  constructor() {
+    this.test = test;
+  }
+}
---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+//Modell for the Environment / World
+class Background {
+    constructor(){
+        this.bg1
+    }
+}
+
+class Game {
+  constructor() {}
+
+  init() {
+    console.log('game init');
+    this.background = new Background();
+  }
+}
